02628ba075cb674f6df84eb49917adaeaa7c9c59
Amine Khaldi <amine.khaldi@reactos.org>
2017-10-24 13:16:07 +0100

    [TRAVIS-CI] Doxy test.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -e
+echo 'Performing the git commit'
 git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}" > /dev/null 2>&1
+echo 'Performing the git push'
 git push --force --quiet "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
